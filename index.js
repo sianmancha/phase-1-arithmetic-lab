7e79d579c5a276a7ba4652eba049a0568891bdbf
Sian Mancha <slmancha20@gmail.com>
2021-10-13 12:18:22 -0500

    commit tree df1f086875eb2d2b67279e5cd34a3f54887f8312

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4;
+
+let max = Math.max(18, 6, 9, 4, 20, 16, 15);
